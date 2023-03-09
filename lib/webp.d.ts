a7b03e3e186c962ec89fb779ea1f4648b67fdbb2
Rouven Wessling <me@rouvenwessling.de>
2023-03-09 14:19:17 +0100

    Create todolist of options to implement

--- lib/webp.d.ts
+++ lib/webp.d.ts
@@ -1,4 +1,19 @@
 export type Options = {
+  preset?: 'default'|'photo'|'picture'|'drawing'|'icon'|'text' // @todo unimplemented
+  quality?: number // @todo unimplemented
+  alphaQuality?: number  // @todo unimplemented
+  method?: 0|1|2|3|4|5|6 // @todo unimplemented
+  size?: number // @todo unimplemented
+  sns?: number // @todo unimplemented
+  filter?: number // @todo unimplemented
+  noAlpha?: boolean // @todo unimplemented
+  autoFilter?: boolean // @todo unimplemented
+  sharpness?: 0|1|2|3|4|5|6|7 // @todo unimplemented
+  lossless?: any // @todo unimplemented. Might split the definition
+  nearLossless?: number // @todo unimplemented
+  crop?: [number, number, number, number] // @todo unimplemented
+  resize?: [number, number] // @todo unimplemented
+  metadata?: any // @todo unimplemented. Need to define how this is passed
 }
 
 export function createWebpSync(input: Buffer, options?: Options) : Buffer;
