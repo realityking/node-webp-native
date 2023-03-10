d4bb0d09bba1b088b52cdbbccf8e028cd03c938a
Rouven Wessling <me@rouvenwessling.de>
2023-03-10 15:29:52 +0100

    Implement test for ESM

--- test/webp.test.mjs
+++ test/webp.test.mjs
@@ -1,3 +1,16 @@
+import { readFile } from 'fs/promises'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 import test from 'ava'
 
 import { convertToWebpSync, convertToWebp } from 'node-webp-native'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+test('ESM sync: convert a WebP image', async t => {
+  const buf = await readFile(path.join(__dirname, 'fixtures/1.webp'))
+  const data = convertToWebpSync(buf)
+
+  t.true(data.length < buf.length)
+})
